55e17c9c55c1969210ba9f4ea99656108c48d53f
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2021-11-14 18:02:54 +0100

    to check

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ImageApiService from './js/apiService';
 import imageTmpl from './templates/markupImageCard';
+import LoadMoreBtn from './js/load-more-bth';
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
@@ -7,26 +8,59 @@ const refs = {
     imageUl: document.querySelector('.gallery'),
 };
 
+const loadMoreBth = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
 const imageApiSearch = new ImageApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBth.addEventListener('click', onLoadMore);
+loadMoreBth.refs.button.addEventListener('click', fetchImageCard);
+console.log(loadMoreBth.refs.button);
 
 function onSearch(e){
   e.preventDefault();
+  
+  imageApiSearch.query = e.currentTarget.elements.query.value;
+
+  if(imageApiSearch.query === ' '){
+    return alert('ВВедите что-то');
+  };
+  loadMoreBth.show();
   clearHitsContainer();
-  imageApiSearch.searchQuery = e.currentTarget.elements.query.value;
   imageApiSearch.resetPage();
-  imageApiSearch.fetchImage().then(appendHitsMarkup);
+  fetchImageCard ();
+  
 };
 
-function onLoadMore (){
-  imageApiSearch.fetchImage().then(appendHitsMarkup);
+function fetchImageCard (){
+  loadMoreBth.disable();
+  imageApiSearch.fetchImage().then(hits =>{
+  appendHitsMarkup(hits);
+  loadMoreBth.enable();
+  });
 };
 
 function appendHitsMarkup(hits) {
   refs.imageUl.insertAdjacentHTML('beforeend', imageTmpl(hits));
 };
 function clearHitsContainer(){
-  refs.imageUl.innerHTML = '';
-}
+  refs.imageUl.innerHTML = ' ';
+};
+const element = document.getElementById('.my-element-selector');
+element.scrollIntoView({
+  behavior: 'smooth',
+  block: 'end',
+});
+
+function scrollPage() {
+  const refGalleryItem = document.querySelectorAll('.gallery__item');
+
+  if (refGalleryItem.length > apiServise.pPage) {
+    refGalleryItem[refGalleryItem.length - apiServise.pPage].scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+};
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,7 +2,7 @@ export default class ImageApiSearch {
     constructor(){
         this.searchQuery = ' ';
         this.page = 1;
-    }
+    };
     fetchImage(){
       console.log(this);
         const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=24297910-266d35e9d32fe9ab4dcc44a53`;
@@ -11,19 +11,22 @@ export default class ImageApiSearch {
         .then(response => 
           response.json())
         .then(({hits}) => {
-          this.page += 1;
+          this.incrementPage();
           return hits;
           }
             );
+    };
+    incrementPage(){
+      this.page +=1;
     }
     resetPage(){
       this.page = 1;
-    }
+    };
     get query() {
         return this.searchQuery;
-      }
+    };
     
     set query(newQuery) {
         this.searchQuery = newQuery;
-      }
-}
+    };
+};
